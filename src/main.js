7276ef88ea5586d12a5ee3d409604f1fb82454fa
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2023-04-19 23:27:52 +0200

    new: start painting from pixels from array [wrong]

--- src/main.js
+++ src/main.js
@@ -4,7 +4,34 @@ const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+const RES = 64;
+
 (async () => {
     const {instance} = await WebAssembly.instantiateStreaming(fetch('build/main.wasm'));
-    console.log(instance.exports.sumInts(4, 1));
+
+    // const rm = instance.exports;
+
+    // let arrPointer = instance.exports.returnArrayOfLength(10);
+    // let arr = new Uint32Array(instance.exports.memory.buffer, arrPointer, 10);
+    // console.log(arr);
+    // console.log(instance.exports.getHeapUsage());
+
+    const worldPointer = instance.exports.createWorld();
+    console.log(instance.exports);
+    let time = performance.now();
+    const raymarchResult = instance.exports.rayMarch(RES, worldPointer);
+
+    const raymarchResultArr = new Int8Array(instance.exports.memory.buffer, raymarchResult, RES * RES * 3);
+
+    console.log("calling wasm took: " + (performance.now() - time));
+    time = performance.now();
+
+    const pixelSize = canvas.width / RES;
+    for (let i = 0; i < RES; i++) {
+        for (let j = 0; j < RES; j++) {
+            ctx.fillStyle = `rgb(${raymarchResultArr[i * RES + j]}, ${raymarchResultArr[i * RES + j + 1]}, ${raymarchResultArr[i * RES + j + 2]})`;
+            ctx.fillRect(j * pixelSize, i * pixelSize, pixelSize - 1, pixelSize - 1);
+        }
+    }
+    console.log("iterating thru pixels took: " + (performance.now() - time));
 })();
