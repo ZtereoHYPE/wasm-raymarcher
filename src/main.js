d3dd3b92441c4406d43580ec9fb3eb333932fd35
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2023-04-26 01:16:35 +0200

    new: refactor everything and start render loop

--- src/main.js
+++ src/main.js
@@ -1,37 +1,55 @@
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d', {alpha: false});
 
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 const RES = 64;
 
-(async () => {
-    const {instance} = await WebAssembly.instantiateStreaming(fetch('build/main.wasm'));
+let wasmModule = null;
 
-    // const rm = instance.exports;
+const memory = new WebAssembly.Memory({initial: 1, maximum: 16});
+fetch('build/main.wasm')
+    .then(response => response.arrayBuffer())
+    .then(buffer => WebAssembly.compile(buffer))
+    .then(compiledModule => {
+        const jslog = (offset, length) => {
+            // let bytes = new Uint8Array(memory.buffer, offset, length);
+        
+            // let string = new TextDecoder('utf8').decode(char);
+        
+            console.log(String.fromCharCode(offset));
+        }
 
-    // let arrPointer = instance.exports.returnArrayOfLength(10);
-    // let arr = new Uint32Array(instance.exports.memory.buffer, arrPointer, 10);
-    // console.log(arr);
-    // console.log(instance.exports.getHeapUsage());
+        wasmModule = new WebAssembly.Instance(compiledModule, {
+            js: {mem: memory},
+            env: { "jslog": jslog }
+        });
+        
+        setup();
+        draw();
+    })
 
-    const worldPointer = instance.exports.createWorld();
-    console.log(instance.exports);
-    let time = performance.now();
-    const raymarchResult = instance.exports.rayMarch(RES, worldPointer);
+let worldPointer = null;
+let exp = null;
 
-    const raymarchResultArr = new Int8Array(instance.exports.memory.buffer, raymarchResult, RES * RES * 3);
+function setup() {
+    exp = wasmModule.exports;
+    worldPointer = exp.createWorld();
+    exp.addSphere(worldPointer, 0, 0, 10, 0.0);
+}
 
-    console.log("calling wasm took: " + (performance.now() - time));
-    time = performance.now();
+function draw() {
+    const raymarchResult = exp.rayMarch(RES, worldPointer);
+    const raymarchResultArr = new Int32Array(exp.memory.buffer, raymarchResult, RES * RES * 3);
 
     const pixelSize = canvas.width / RES;
     for (let i = 0; i < RES; i++) {
         for (let j = 0; j < RES; j++) {
-            ctx.fillStyle = `rgb(${raymarchResultArr[i * RES + j]}, ${raymarchResultArr[i * RES + j + 1]}, ${raymarchResultArr[i * RES + j + 2]})`;
-            ctx.fillRect(j * pixelSize, i * pixelSize, pixelSize - 1, pixelSize - 1);
+            ctx.fillStyle = `rgb(${raymarchResultArr[(i * RES + j) * 3]}, ${raymarchResultArr[(i * RES + j) * 3 + 1]}, ${raymarchResultArr[(i * RES + j) * 3 + 2]})`;
+            ctx.fillRect(j * pixelSize + 1, i * pixelSize + 1, pixelSize - 1, pixelSize - 1);
         }
     }
-    console.log("iterating thru pixels took: " + (performance.now() - time));
-})();
+
+    // requestAnimationFrame(draw);
+}
