fafd262f4aa0d2f19a9c8fcd55646cfb2a4a64e3
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2023-04-29 14:56:36 +0200

    new: 6 axis movement + fps logg

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d', {alpha: false});
+const ctx = canvas.getContext('2d', {alpha: false, antialias: false});
 
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -33,7 +33,7 @@ let pressedKeys = [];
 function setup() {
     exp = wasmModule.exports;
     worldPointer = exp.createWorld();
-    exp.addSphere(worldPointer, 0, 0, 10, 10.0);
+    exp.addSphere(worldPointer, 0, 0, 20, 10.0);
 
     window.addEventListener('keydown', (e) => {
         pressedKeys[e.keyCode] = true;
@@ -46,23 +46,26 @@ function setup() {
 function draw() {
     movePlayer();
 
+    let time = performance.now();
+
     const raymarchResult = exp.rayMarch(RES, worldPointer);
     const raymarchResultArr = new Int32Array(exp.memory.buffer, raymarchResult, RES * RES * 3);
-
     const pixelSize = canvas.width / RES;
     for (let i = 0; i < RES; i++) {
         for (let j = 0; j < RES; j++) {
             ctx.fillStyle = `rgb(${raymarchResultArr[(i * RES + j) * 3]}, ${raymarchResultArr[(i * RES + j) * 3 + 1]}, ${raymarchResultArr[(i * RES + j) * 3 + 2]})`;
-            ctx.fillRect(j * pixelSize + 1, i * pixelSize + 1, pixelSize - 1, pixelSize - 1);
+            ctx.fillRect(j * pixelSize, i * pixelSize, pixelSize, pixelSize);
         }
     }
+
+    console.log("FPS: " + Math.floor(1000/(performance.now() - time)));
 
     requestAnimationFrame(draw);
 }
 
 // move player using wasd
 function movePlayer() {
-    const speed = 0.1;
+    const speed = 1;
 
     if (pressedKeys[87]) {
         exp.moveCamera(worldPointer, 0, 0, speed);
@@ -75,5 +78,11 @@ function movePlayer() {
     }
     if (pressedKeys[68]) {
         exp.moveCamera(worldPointer, 0, speed, 0);
+    }
+    if (pressedKeys[81]) {
+        exp.moveCamera(worldPointer, speed, 0, 0);
+    }
+    if (pressedKeys[69]) {
+        exp.moveCamera(worldPointer, -speed, 0, 0);
     }
 }
